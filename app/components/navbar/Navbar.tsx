ea43931cd4ab37745841334382ee08581d042012
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-03 14:58:09 -0400

    Handle hydration errors

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Container from "../Container";
 import Logo from "./Logo"
 import Search from "./Search";
@@ -5,7 +7,7 @@ import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div className="fixed w-full bg-green-800/10 z-10 shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
