b444db9f287b00c114873dbbf13c1caa65090e72
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-04 14:42:19 -0400

    Add CategoryBox component for handling URL navigation and query params

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,60 @@
+import { useRouter, useSearchParams } from "next/navigation"
+import { useCallback } from "react";
+import { IconType } from "react-icons"
+import qs from "query-string"
+
+interface CategoryBoxProps {
+    icon: IconType;
+    label: string;
+    selected?: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({ icon: Icon, label, selected }) => {
+
+    // Hooks for handling URL navigation and query params
+    const router = useRouter();
+    const params = useSearchParams();
+
+    // A callback function to handle click events on the component
+    const handleClick = useCallback(() => {
+
+        // Retrieve the current query params and parse them into an object
+        let currentQuery = {};
+        if (params) {
+          currentQuery = qs.parse(params.toString())
+        }
+
+        // Update the `category` property in the query params
+        const updatedQuery: any = {
+          ...currentQuery,
+          category: label
+        }
+
+        // If the selected category is already in the URL, remove it
+        if (params?.get('category') === label) {
+          delete updatedQuery.category;
+        }
+
+        // Generate a URL with the updated query params
+        const url = qs.stringifyUrl({
+          url: '/',
+          query: updatedQuery
+        }, { skipNull: true });
+
+        // Navigate to the new URL
+        router.push(url);
+
+      }, [label, router, params]);
+
+    // Return the rendered component with dynamic styling based on `selected` prop
+    return (
+        <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? 'border-b-neutral-800' : 'border-transparent'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}>
+            <Icon size={26} />
+            <div className="font-medium text-sm">
+                {label}
+            </div>
+        </div>
+    )
+}
+
+export default CategoryBox;
---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,46 @@
+import Container from "../Container"
+
+import { TbBeach } from 'react-icons/tb'
+import { GiWindmill } from 'react-icons/gi'
+import { MdOutlineVilla } from 'react-icons/md'
+import CategoryBox from "../CategoryBox"
+import { useSearchParams } from "next/navigation"
+
+export const categories = [
+    {
+        label: 'Beach',
+        icon: TbBeach,
+        description: 'This property is close to the beach!'
+    },
+    {
+        label: 'Windmills',
+        icon: GiWindmill,
+        description: 'This property has windmills!'
+    },
+    {
+        label: 'Modern',
+        icon: MdOutlineVilla,
+        description: 'This property has a modern look!'
+    },
+]
+
+const Categories = () => {
+    const params = useSearchParams();
+    const category = params?.get('category');
+    return (
+        <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+                {categories.map((item) => (
+                    <CategoryBox
+                        key={item.label}
+                        label={item.label}
+                        description={item.description}
+                        icon={item.icon}
+                    />
+                ))}
+            </div>
+        </Container>
+    )
+}
+
+export default Categories
