d695f42606ab4ba9c3a5c5feba561c1d9f6e57ce
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-04 13:55:35 -0400

    Implement Github auth

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import Heading from '../Heading';
 import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -86,7 +87,7 @@ const RegisterModal = () => {
         <div className='justify-center items-center flex flex-col gap-4 mt-3'>
             <hr />
             <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-            <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+            <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn('github')} />
             <div className='text-neutral-500 text-center mt-4 font-light'>
                 <div className='flex flex-row items-center gap-2'>
                     <div>
