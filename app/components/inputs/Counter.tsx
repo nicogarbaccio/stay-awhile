b292d1c6cb3308a4e1d59009e67c407590002b92
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-05 11:12:47 -0400

    Set up and implement Cloudinary

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -3,6 +3,7 @@
 import { useCallback } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
+// Define the "CounterProps" interface for the component's props
 interface CounterProps {
     title: string;
     subtitle: string;
@@ -10,11 +11,14 @@ interface CounterProps {
     onChange: (value: number) => void;
 }
 
+// Create the "Counter" functional component with "CounterProps"
 const Counter: React.FC<CounterProps> = ({ title, subtitle, value, onChange }) => {
+    // Create a memoized "onAdd" function to increment the counter value
     const onAdd = useCallback(() => {
         onChange(value + 1);
     }, [onChange, value])
 
+    // Create a memoized "onReduce" function to decrement the counter value
     const onReduce = useCallback(() => {
         if (value === 1) {
             return;
@@ -22,6 +26,7 @@ const Counter: React.FC<CounterProps> = ({ title, subtitle, value, onChange }) =
         onChange(value - 1);
     }, [value, onChange])
 
+    // Render the component
     return (
         <div className="flex flex-row items-center justify-between">
             <div className="flex flex-col">
@@ -33,14 +38,15 @@ const Counter: React.FC<CounterProps> = ({ title, subtitle, value, onChange }) =
                 </div>
             </div>
             <div className="flex flex-row items-center gap-4">
-                <div onClick={onReduce} 
+                <div onClick={onReduce} // Attach the "onReduce" function to the onClick event
                 className="w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition">
                 <AiOutlineMinus />
                 </div>
                 <div className="font-light text-xl text-neutral-600">
+                    {/* Display the current counter value */}
                     {value}
                 </div>
-                <div onClick={onAdd} 
+                <div onClick={onAdd} // Attach the "onAdd" function to the onClick event
                 className="w-10 h-10 rounded-full border-[1px] border-neutral-400 flex items-center justify-center text-neutral-600 cursor-pointer hover:opacity-80 transition">
                 <AiOutlinePlus />
                 </div>
