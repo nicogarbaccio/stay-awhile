b1040fcb2d2dcd608468cbfd03af3f5667ded676
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-06 18:22:13 -0400

    User can successfully delete a property

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -54,7 +54,7 @@ const UserMenu: React.FC<UserMenuProps> = ( { currentUser } ) => {
                             <MenuItem onClick={() => router.push('/trips')} label="My trips" />
                             <MenuItem onClick={() => router.push('/favorites')} label="My favorites" />
                             <MenuItem onClick={() => router.push('/reservations')} label="My reservations" />
-                            <MenuItem onClick={() => {}} label="My properties" />
+                            <MenuItem onClick={() => router.push('/properties')} label="My properties" />
                             <MenuItem onClick={rentModal.onOpen} label="Become a host" />
                             <hr />
                             <MenuItem onClick={() => signOut()} label="Log out" />
