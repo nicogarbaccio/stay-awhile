85a7735b4d04e78ddf51fab270ba0a2d61d1943a
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-05 22:48:51 -0400

    Render map in individual listing page

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -5,6 +5,11 @@ import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../Map"), {
+    ssr: false
+});
 
 interface ListingInfoProps {
     user: SafeUser;
@@ -54,6 +59,12 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                     description={category.description}
                 />
             )}
+            <hr />
+            <div className="text-lg font-light text-neutral-500">
+                {description}
+            </div>
+            <hr />
+            <Map center={coordinates} />
         </div>
     )
 }
