102199cc11f10755036e8dfb77917c04fd02c9e1
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-06 23:20:59 -0400

    Styled navbar and fix overflow on mobile

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -110,7 +110,7 @@ const Categories = () => {
 
     return (
         <Container>
-            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+            <div className="pt-4 flex flex-row items-center bg-white justify-between overflow-x-auto">
                 {categories.map((item) => (
                     <CategoryBox
                         key={item.label}
