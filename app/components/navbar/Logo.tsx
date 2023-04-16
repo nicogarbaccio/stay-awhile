8ddc3b176bb155790ecd67d0ddb73b88605f0499
Nico Garbaccio <garbaccio20@gmail.com>
2023-04-16 10:19:34 -0400

    Test changing route on logo click for use later on

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -2,6 +2,22 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation";
+import { SafeUser } from "@/app/types";
+
+// Save this for later!
+// interface LogoProps {
+//     currentUser?: SafeUser | null;
+// }
+
+// const Logo: React.FC<LogoProps> = ({currentUser}) => {
+//     const router = useRouter();
+
+//     return (
+//         <h1 onClick={() => {currentUser?.favoriteIds !== null ? router.push('/favorites') : router.push('/')}} className="hidden md:block cursor-pointer text-xl text-green-800 font-extrabold">
+//             🏡 STAY AWHILE
+//         </h1>
+//     )
+// }
 
 const Logo = () => {
     const router = useRouter();
